041bb5fc3409f0011ef6a0371928c5a6d8afbb8e
abelash <abelash@gmail.com>
2020-02-04 18:32:16 +0200

    Initial Commit

---
+++ src/test/scala/org/abelash/demo/SpartBQTestConnector.scala
@@ -0,0 +1,17 @@
+package org.abelash.demo
+
+import org.junit._
+import Assert._
+
+@Test
+class SpartBQTestConnector {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
