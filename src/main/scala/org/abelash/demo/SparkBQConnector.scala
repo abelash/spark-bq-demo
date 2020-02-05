7699c24a84040ce8e0faa4e1d63e7b2efd626024
abelash <abelash@gmail.com>
2020-02-05 15:48:35 +0200

    Success writes!

---
+++ src/main/scala/org/abelash/demo/SparkBQConnector.scala
@@ -0,0 +1,52 @@
+package org.abelash.demo
+
+
+/**
+ * Hello world!
+ *
+ */
+/*
+gcloud dataproc jobs submit spark --cluster "$MY_CLUSTER" \
+  --jar=target/abe-spark-bq-demo-0.1.jar \
+  --jars gs://spark-lib/bigquery/spark-bigquery-latest.jar
+*/
+object SparkBQConnector  {
+
+  def main(args: Array[String]) {
+    println("Hello World!")
+
+    /*
+ * Remove comment if you are not running in spark-shell.
+ * */
+    import org.apache.spark.sql.SparkSession
+    val spark = SparkSession.builder()
+      .appName("spark-bigquery-demo")
+      .getOrCreate()
+
+    // Use the Cloud Storage bucket for temporary BigQuery export data used
+    // by the connector.
+    val bucket = "abe-bq-writer"
+    spark.conf.set("temporaryGcsBucket", bucket)
+
+    // Load data in from BigQuery.
+    val wordsDF = spark.read.format("bigquery")
+      .option("table", "publicdata.samples.shakespeare")
+      .load()
+      .cache()
+    wordsDF.createOrReplaceTempView("words")
+
+    // Perform word count.
+    val wordCountDF = spark.sql(
+      "SELECT word, SUM(word_count) AS word_count FROM words GROUP BY word")
+    wordCountDF.show()
+    wordCountDF.printSchema()
+
+    println("Starting saving to BQ")
+    // Saving the data to BigQuery.
+    wordCountDF.write
+      .format("bigquery")
+      .option("table", "wordcount_dataset.wordcount_output")
+      .save()
+    println("Starting to BQ COMPLETED")
+  }
+}
